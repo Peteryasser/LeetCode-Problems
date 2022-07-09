f964f4f4502c2f55bc7b021d96a32c8368399daf
Peteryasser <96436582+Peteryasser@users.noreply.github.com>
2022-07-09 02:17:47 +0200

    Time: 5 ms (74.90%), Space: 43.8 MB (54.57%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())return false;
-        HashMap<Character,Integer> h=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            h.put(s.charAt(i),(h.getOrDefault(s.charAt(i),0)+1));
-        }
-        for(int i=0;i<t.length();i++){
-            if(!h.containsKey(t.charAt(i)))return false;
-            h.put(t.charAt(i),h.get(t.charAt(i))-1);
-            if(h.get(t.charAt(i))==0)h.remove(t.charAt(i));
-        }
+        int[] ch = new int[26];
+        for(char c: s.toCharArray()) ch[c - 'a']++;
+        for(char c: t.toCharArray()) ch[c - 'a']--;
+        for(int i: ch)
+            if(i != 0)
+                return false;
         return true;
     }
 }
